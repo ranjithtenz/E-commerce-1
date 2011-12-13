778952c9a67a86db2cd71970fc54b9a2f2f3fecb
Said Kaldybaev <said.kaldybaev@gmail.com>
2011-12-13 22:22:49 +0600

    Default Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'c36c973fa706a706c7982214eaf6babe24522fccaae6cba43426be5b3b7b94c323a10f63562f09a1d63db34ae165ba6ab91df748c0d5acb3120f031402060872'
