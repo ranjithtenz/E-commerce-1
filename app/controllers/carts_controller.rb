1064e1064778679f854626ec146e1fc881fa625b
Said Kaldybaev <said.kaldybaev@gmail.com>
2011-12-17 21:17:43 +0600

    Authentication and I18n support added

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  skip_before_filter :authorize, :only => [:create, :update, :destroy]
 
   def index
     @carts = Cart.all
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,5 @@
 class LineItemsController < ApplicationController
+  skip_before_filter :authorize, :only => :create
 
   def index
     @line_items = LineItem.all
